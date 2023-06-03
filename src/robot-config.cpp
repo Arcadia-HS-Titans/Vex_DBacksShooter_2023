3a456e78893f222a766f5283c85db7ccc3f7643e
3187 RObotics Team <titanrobotics3187@gmail.com>
2023-06-03 12:32:33 -0700

    Made it speedily turn (~12 seconds)

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -10,8 +10,8 @@ using code = vision::code;
 brain Brain;
 
 // VEXcode device constructors
-motor leftDrive = motor(PORT7, ratio18_1, false);
-motor rightDrive = motor(PORT8, ratio18_1, true);
+motor leftDrive = motor(PORT1, ratio18_1, false);
+motor rightDrive = motor(PORT2, ratio18_1, true);
 gyro TurnGyroSmart = gyro(Brain.ThreeWirePort.D);
 smartdrive Drivetrain = smartdrive(leftDrive, rightDrive,
                                    TurnGyroSmart, 319.19, 320, 130, mm, 1);
@@ -28,6 +28,8 @@ smartdrive Drivetrain = smartdrive(leftDrive, rightDrive,
 static bool stopVexProgram = false;
 long long currentTime = 0;
 Timer checkErrors{"AAA"};
+Timer programLoop{"Time to complete: "};
+double totDegrees = 0;
 
 void startSecondThread() {
   while(!stopVexProgram) {
@@ -41,12 +43,39 @@ void startSecondThread() {
   }
 }
 
+void multiDrive(double totDegrees) {
+}
+
 void vexcodeInit(void) {
   checkErrors.start();
   thread errorChecker{startSecondThread};
+  programLoop.start();
+  int tick = 0;
   while(!stopVexProgram) {
+    if(tick >= 4) {
+      stopVexProgram = true;
+      break;
+    }
     // Loop inside here
-    leftDrive.spinFor(90, degrees);
+    // 30 * 3.14 * 1/4 = each wheel
+    // 180 degrees = 10.5
+    double randomAdjuster = 0;
+
+    double distanceBetweenWheels = 29.5; // Centimeters
+    double wheelDiameter = 10.5; // Centimeters
+    double distance = distanceBetweenWheels * M_PI_2;
+    double rotations = distance / wheelDiameter;
+    totDegrees = (rotations * 180. / M_PI) + randomAdjuster; // The 5 is to adjust for some random offset
+
+    Drivetrain.setDriveVelocity(75, percent);
+    Drivetrain.driveFor(6*11, inches);
+    Drivetrain.setDriveVelocity(20, percent);
+    thread dd{[](){leftDrive.spinFor(-totDegrees, degrees);}};
+    rightDrive.spinFor(totDegrees, degrees);
     currentTime = checkErrors.getTime();
+    tick++;
+   //rightDrive.spinFor(1.34159203, rev);
   }
+  double totalTime = (((double) programLoop.getTime()) / 1000.);
+  Brain.Screen.printAt(10, 40, "Total Time: %f", totalTime);
 }
